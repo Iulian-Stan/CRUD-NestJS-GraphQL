4a993a64be1671d022c81a1cc7f2a6f9b7263be5
Iulian-Stan <moldova.rules@yahoo.com>
2022-11-27 16:54:00 +0200

    Added configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     BooksModule,
     UsersModule,
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
     SequelizeModule.forRoot({
       dialect: 'sqlite',
-      storage: './database.sqlite',
+      storage: 'database.sqlite',
       autoLoadModels: true,
       synchronize: true
     }),
